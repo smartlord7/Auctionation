6a5f5de52a23bf6b6b6f52aa634ff38fb8c46c20
Sancho Amaral Simões <ssimoes@ipn.pt>
2021-05-27 14:42:33 +0100

    MAIN: Configure project.

---
+++ source_code/src/test/java/auctionaction/source_code/SourceCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package auctionaction.source_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
